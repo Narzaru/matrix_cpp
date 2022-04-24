85c235197aa75bc1fe4b3a0486b57d277044658b
Narzaru <yaroslahek@gmail.com>
2022-04-24 18:04:51 +0700

    add forgoten file

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -4,26 +4,60 @@
 #include <cmath>
 #include "matrix.h"
 
-S21Matrix::S21Matrix() : m_rows {3}, m_columns {3}, m_matrix {new double[m_rows * m_columns]} {
+
+// Base constructor with no arguments.
+// It create a 3x3 zero matrix.
+S21Matrix::S21Matrix()
+  : m_rows(3),
+    m_columns(3),
+    m_matrix(new double[m_rows * m_columns]) {
   std::fill<double *>(m_matrix, m_matrix + (m_rows * m_columns), 0.0);
 }
 
-S21Matrix::S21Matrix(int rows, int cols) : m_rows {rows}, m_columns {cols}, m_matrix {nullptr} {
+// Constructor with two arguments, rows and columns.
+// It create a RxC zero matrix
+S21Matrix::S21Matrix(int rows, int cols)
+  : m_rows(rows),
+    m_columns(cols),
+    m_matrix(nullptr) {
   if (m_rows > 0 && m_columns > 0) {
     m_matrix = new double[m_rows * m_columns];
-    std::fill<double *>(m_matrix, m_matrix + (m_rows * m_columns), 0.0);
+    std::fill(m_matrix, m_matrix + (m_rows * m_columns), 0.0);
   } else {
     throw std::invalid_argument("zero matrix size");
+  }
+}
+
+// Copy constructor with const S21Matrix& argument.
+// It allocates a new matrix and copy other matrix.
+S21Matrix::S21Matrix(const S21Matrix& other)
+  : m_rows(other.m_rows),
+    m_columns(other.m_columns),
+    m_matrix(nullptr) {
+  if (other.m_matrix != nullptr) {
+    m_matrix = new double[m_rows * m_columns];
+    for (int i = 0; i < m_rows; ++i) {
+      for (int j = 0; j < m_columns; ++j) {
+        m_matrix[i + j + i * (m_columns - 1)] = other.m_matrix[i + j + i * (m_columns - 1)];
+      }
+    }
   }
 }
 
 S21Matrix::~S21Matrix() {
   m_rows = 0;
   m_columns = 0;
-  if (m_matrix != nullptr) {
-    delete [] m_matrix;
-    m_matrix = nullptr;
-  }
+  delete [] m_matrix;
+  m_matrix = nullptr;
+}
+
+S21Matrix::S21Matrix(S21Matrix&& other)
+  : m_rows(other.m_rows),
+    m_columns(other.m_columns),
+    m_matrix(other.m_matrix) {
+  other.m_matrix = nullptr;
+  other.m_rows = 0;
+  other.m_columns = 0;
 }
 
 int S21Matrix::get_rows() const {
@@ -34,7 +68,7 @@ int S21Matrix::get_columns() const {
   return m_columns;
 }
 
-bool S21Matrix::eq_matrix(const S21Matrix& other) const{
+bool S21Matrix::eq_matrix(const S21Matrix& other) const {
   bool is_same = true;
   if (m_rows == other.m_rows && m_columns == other.m_columns) {
     for (int i = 0; i < m_rows * m_columns; ++i) {
@@ -51,7 +85,7 @@ bool S21Matrix::eq_matrix(const S21Matrix& other) const{
 double& S21Matrix::operator()(int i, int j) {
   if (m_matrix != nullptr) {
     if (i >= 0 && i < m_rows && j >= 0 && j < m_columns) {
-      return m_matrix[i + j + i * m_columns];
+      return m_matrix[i + j + i * (m_columns - 1)];
     } else {
       throw std::out_of_range("indexes out of range");
     }
@@ -60,6 +94,6 @@ double& S21Matrix::operator()(int i, int j) {
   }
 }
 
-bool S21Matrix::is_empty() const {
+bool S21Matrix::is_empty_object() const {
   return (m_matrix == nullptr);
 }
