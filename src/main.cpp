ab3404002985a7b2abab1e29c09d8b4df75496ce
Narzaru <yaroslahek@gmail.com>
2022-04-29 19:05:52 +0700

    test main suite

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,12 @@
 
 int main() {
   try {
-    S21Matrix matrix1(2, 2);
-    matrix1.set_values({1, 2, 3, 4});
-    S21Matrix new_matrix = matrix1.calc_complements();
-    std::cout << new_matrix;
+    S21Matrix matrix1(3, 2);
+    S21Matrix matrix2(3, 2);
+    matrix1.set_values({1, 2, 3, 4, 5, 6});
+    matrix2.set_values({1, 2, 3, 4, 5, 6});
+    S21Matrix matrix3 = matrix1 + matrix2;
+    std::cout << matrix3;
   } catch(const std::exception& e) {
     std::cerr << e.what() << std::endl;
   }
