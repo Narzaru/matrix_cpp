b6769925bd44eb32d322a202ce94f73991b2d8bd
Narzaru <yaroslahek@gmail.com>
2022-05-01 20:51:45 +0700

    fix typo

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,12 @@
 
 int main() {
   try {
-    // S21Matrix matrix1(1, 2);
-    // S21Matrix matrix2(2, 1);
-    // matrix1.set_values({1, 10});
-    // matrix2.set_values({3, 4});
-    // S21Matrix matrix3(std::move(matrix1 * matrix2));
-    // std::cout << matrix1 << std::endl << matrix2 << std::endl << matrix3;
-
-    S21Matrix matrix1;
-    (matrix1 = matrix1);
-    S21Matrix matrix2;
-    (matrix2 = matrix1);
-    S21Matrix matrix3(4, 4);
-    (matrix1 = matrix3);
+    S21Matrix matrix1(1, 2);
+    S21Matrix matrix2(2, 1);
+    matrix1.set_values({1, 10});
+    matrix2.set_values({3, 4});
+    S21Matrix matrix3(std::move(matrix1 * matrix2));
+    std::cout << matrix1 << std::endl << matrix2 << std::endl << matrix3;
   } catch(const std::exception& e) {
     std::cerr << e.what() << std::endl;
   }
