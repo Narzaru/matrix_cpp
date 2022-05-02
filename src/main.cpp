ec718fa9f5d0fcdf468fd54ab0dc83256702756c
Narzaru <yaroslahek@gmail.com>
2022-05-02 18:08:04 +0700

    return main example

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,12 @@
 
 int main() {
   try {
-    // S21Matrix matrix1(1, 2);
-    // S21Matrix matrix2(2, 1);
-    // matrix1.set_values({1, 10});
-    // matrix2.set_values({3, 4});
-    // S21Matrix matrix3(std::move(matrix1 * matrix2));
-    // std::cout << matrix1 << std::endl << matrix2 << std::endl << matrix3;
-    S21Matrix matrix(matrix);
+    S21Matrix matrix1(1, 2);
+    S21Matrix matrix2(2, 1);
+    matrix1.set_values({1, 10});
+    matrix2.set_values({3, 4});
+    S21Matrix matrix3(std::move(matrix1 * matrix2));
+    std::cout << matrix1 << std::endl << matrix2 << std::endl << matrix3;
   } catch(const std::exception& e) {
     std::cerr << e.what() << std::endl;
   }
